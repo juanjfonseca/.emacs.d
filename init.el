db898998231d4f4b5278bd465c4ad9e1adb89496
Juan Fonseca <hvxt37@motorolasolutions.com>
2016-02-05 09:10:40 -0500

    Turn on global-hl-line-mode.

--- init.el
+++ init.el
@@ -93,6 +93,7 @@
 ;;; Customizations
 (blink-cursor-mode -1)
 (column-number-mode t)
+(global-hl-line-mode t)
 (global-linum-mode t)
 (setq delete-by-moving-to-trash t) ; Move deleted files to Recycle.
 (setq visible-bell t)
