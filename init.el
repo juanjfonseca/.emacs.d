0dfe08292f4912562106c507d99f286bd6beb363
Juan Fonseca <hvxt37@motorolasolutions.com>
2016-04-28 14:12:35 -0400

    Turn off Global-hl-line-mode.

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 (load-theme 'wombat t)
 (blink-cursor-mode -1)
 (column-number-mode t)
-(global-hl-line-mode t)
 (global-linum-mode t)
 (setq delete-by-moving-to-trash t) ; Move deleted files to Recycle.
 (setq visible-bell t)
