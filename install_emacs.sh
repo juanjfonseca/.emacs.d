ca5d521b91e566848c43333d6caae0e003202533
Juan Fonseca <hvxt37@motorolasolutions.com>
2016-12-07 15:04:03 -0500

    Added install_emacs.sh

---
+++ install_emacs.sh
@@ -0,0 +1,4 @@
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/emacs 1
+sudo update-alternatives --set editor /usr/local/bin/emacs
+sudo update-alternatives --install /usr/bin/emacs emacs /usr/local/bin/emacs 1
+sudo update-alternatives --set emacs /usr/local/bin/emacs
