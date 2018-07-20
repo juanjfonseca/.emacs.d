08c5445c0ea89c18b45aa8091fd2cf4eab7d3b43
Juan Fonseca <hvxt37@motorolasolutions.com>
2018-07-20 15:28:08 -0400

    set font to Courier 10 Pitch

--- init.el
+++ init.el
@@ -242,6 +242,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Courier 10 Pitch" :foundry "bitstream" :slant normal :weight normal :height 113 :width normal)))))
 (provide 'init)
 ;;; init.el ends here
